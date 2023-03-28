6672d42e7ce2f6045637a7b026fb6aa48fed049e
Giorgio Rosolia <giorgiorosolia@AirdiGiorgio2.homenet.telecomitalia.it>
2023-03-28 21:29:50 +0200

    first commit

---
+++ client/index.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+const verifyProof = require('../utils/verifyProof');
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+  const name = 'ciao';
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  console.log( verifyProof(proof, name, root) )
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    // TODO: add request body parameters here!
+    proof,
+    name,
+    root
+  });
+
+  console.log({ gift });
+}
+
+main();
